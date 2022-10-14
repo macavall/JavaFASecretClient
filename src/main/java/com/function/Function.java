ddea795f97346d99f392f434b508e0f1ec35f39d
Matthew Cavallo <macavall@microsoft.com>
2022-10-13 19:49:12 -0500

    First

---
+++ src/main/java/com/function/Function.java
@@ -0,0 +1,71 @@
+package com.function;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+import com.azure.identity.DefaultAzureCredential;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("HttpExample")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
+        createDefaultAzureCredential(context);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
+    }
+
+    /**
+     * The default credential first checks environment variables for configuration.
+     * If environment configuration is incomplete, it will try managed identity.
+     */
+    public void createDefaultAzureCredential(ExecutionContext context) {
+        DefaultAzureCredential defaultCredential = new DefaultAzureCredentialBuilder().build();
+
+        // Azure SDK client builders accept the credential as a parameter
+        SecretClient client = new SecretClientBuilder()
+            .vaultUrl("https://manidenttest561kv.vault.azure.net")
+            .credential(defaultCredential)
+            .buildClient();
+
+            KeyVaultSecret secretValue = client.getSecret("secret");
+
+            String mySecretValue = secretValue.getValue();
+            
+            context.getLogger().info(mySecretValue);
+    }
+}
